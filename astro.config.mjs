a843108601446b5bf295e659431d289886e5e705
Aaron Ferrer <aaronferrerbarbas@gmail.com>
2025-02-17 20:04:49 +0100

    netlify

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,9 +9,10 @@ import sitemap from "@astrojs/sitemap";
 
 import partytown from "@astrojs/partytown";
 
+
 export default defineConfig({
   // change the 'site' value to your site's domain
-  site: "https://nayasinghania.com",
+  site: "https://deborah-martin-redondo.netlify.app",
   image: {
     domains: ["raw.githubusercontent.com"],
   },
